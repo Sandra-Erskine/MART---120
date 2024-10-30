a5c348b5a055abe85d9f5ed5ad11822717386ba8
Sandra-Erskine <se134537@umconnect.umt.edu>
2024-10-29 18:37:36 -0600

    Add files via upload
    I was late to the cahoot quiz and I could not complete it. let me know if There is a separate way for me to access the quiz

---
+++ sketch.js
@@ -0,0 +1,66 @@
+function setup() {
+  createCanvas(400, 400);
+  background(255); // Set background to white
+}
+
+function draw() {
+   fill(109, 39, 0);
+  noStroke();
+  ellipse(200, 255, 210, 310);
+  fill(213, 187, 161);
+  stroke(139,0,0)
+  ellipse(200, 200, 150, 170);
+  fill(213, 187, 161);
+  ellipse(110, 200, 30, 60)
+  fill(213,187,161)
+  ellipse(290,200,30,60)
+  fill(173, 216, 230)
+  ellipse(230,197,25,15)
+  fill(173, 216, 230)
+  ellipse(170,197,25,15)
+   fill(240, 147, 161);
+  stroke(230, 157, 161);           
+  triangle(200, 200, 200, 240, 220, 220)
+  stroke(130,0,0);        
+  strokeWeight(3);  
+  line(180, 255, 220, 250);
+  stroke(130,0,0);        
+  strokeWeight(3);  
+  line (190, 185, 140, 180)
+  stroke(130,0,0);        
+  strokeWeight(3);  
+  line (260, 175, 210, 185)
+    stroke(0);
+  strokeWeight(2); 
+  noFill()
+   rect(140, 188, 50, 30)
+      stroke(0);
+  strokeWeight(2); 
+  noFill()
+   rect(210, 188, 50, 30)
+  line (210, 200, 190, 200)
+  point(220, 210, 180,)
+  point(230, 210, 180,)
+  point(240, 210, 180,)
+  point(250, 210, 180,)
+  point(220, 220, 190,)
+  point(230, 220, 190,)
+  point(240, 220, 190,)
+  point(250, 220, 190,)
+  point(180, 210, 180,)
+  point(170, 210, 180,)
+  point(160, 210, 180,)
+  point(150, 210, 180,)
+  point(150, 220, 180,)
+  point(160, 220, 190,)
+  point(170, 220, 190,)
+  point(180, 220, 190,)
+     fill(109, 39, 0);
+  noStroke();
+  ellipse(200, 130, 120, 70);
+  fill(0);
+  textSize(32);
+  textAlign(CENTER, TOP);
+  text("SELF PORTRAIT", width / 2, height / 10);
+}
+
