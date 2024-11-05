2d7e5c07fbb1499d0187f390178bc2948e9b7961
Sandra-Erskine <se134537@umconnect.umt.edu>
2024-11-04 22:25:05 -0800

    Homework 10

--- sketch.js
+++ sketch.js
@@ -1,66 +1,57 @@
+let eyeLeftX, eyeRightX, noseY, mouthY, titleSize;
+let eyeLeftSpeed, eyeRightSpeed, noseSpeed, mouthSpeed, diagonalSpeed;
+let diagonalX, diagonalY;
+
 function setup() {
   createCanvas(400, 400);
-  background(255); // Set background to white
+  background(255);
+  eyeLeftX = 110;
+  eyeRightX = 290;
+  noseY = 200;
+  mouthY = 255;
+  titleSize = 32;
+  eyeLeftSpeed = random(1, 3);
+  eyeRightSpeed = random(1, 3);
+  noseSpeed = random(1, 3);
+  mouthSpeed = random(1, 3);
+  diagonalSpeed = random(1, 3);
+  diagonalX = 200;
+  diagonalY = 130;
 }
 
 function draw() {
-   fill(109, 39, 0);
-  noStroke();
-  ellipse(200, 255, 210, 310);
+  background(255);
   fill(213, 187, 161);
-  stroke(139,0,0)
-  ellipse(200, 200, 150, 170);
-  fill(213, 187, 161);
-  ellipse(110, 200, 30, 60)
-  fill(213,187,161)
-  ellipse(290,200,30,60)
-  fill(173, 216, 230)
-  ellipse(230,197,25,15)
-  fill(173, 216, 230)
-  ellipse(170,197,25,15)
-   fill(240, 147, 161);
-  stroke(230, 157, 161);           
-  triangle(200, 200, 200, 240, 220, 220)
-  stroke(130,0,0);        
-  strokeWeight(3);  
-  line(180, 255, 220, 250);
-  stroke(130,0,0);        
-  strokeWeight(3);  
-  line (190, 185, 140, 180)
-  stroke(130,0,0);        
-  strokeWeight(3);  
-  line (260, 175, 210, 185)
-    stroke(0);
-  strokeWeight(2); 
-  noFill()
-   rect(140, 188, 50, 30)
-      stroke(0);
-  strokeWeight(2); 
-  noFill()
-   rect(210, 188, 50, 30)
-  line (210, 200, 190, 200)
-  point(220, 210, 180,)
-  point(230, 210, 180,)
-  point(240, 210, 180,)
-  point(250, 210, 180,)
-  point(220, 220, 190,)
-  point(230, 220, 190,)
-  point(240, 220, 190,)
-  point(250, 220, 190,)
-  point(180, 210, 180,)
-  point(170, 210, 180,)
-  point(160, 210, 180,)
-  point(150, 210, 180,)
-  point(150, 220, 180,)
-  point(160, 220, 190,)
-  point(170, 220, 190,)
-  point(180, 220, 190,)
-     fill(109, 39, 0);
   noStroke();
-  ellipse(200, 130, 120, 70);
+  ellipse(eyeLeftX, 200, 30, 60);
+  ellipse(eyeRightX, 200, 30, 60);
+  eyeLeftX += eyeLeftSpeed;
+  eyeRightX += eyeRightSpeed;
+  if (eyeLeftX > 400 || eyeLeftX < 0) eyeLeftSpeed *= -1;
+  if (eyeRightX > 400 || eyeRightX < 0) eyeRightSpeed *= -1;
+  fill(213, 187, 161);
+  ellipse(200, noseY, 30, 60); 
+  noseY += noseSpeed;
+  if (noseY > 300 || noseY < 100) noseSpeed *= -1;
+  fill(240, 147, 161);
+  ellipse(200, mouthY, 150, 30);
+  mouthY += mouthSpeed;
+  if (mouthY > 300 || mouthY < 250) mouthSpeed *= -1;
+  fill(109, 39, 0);
+  ellipse(diagonalX, diagonalY, 120, 70);
+  diagonalX += diagonalSpeed;
+  diagonalY += diagonalSpeed;
+  if (diagonalX > 400 || diagonalX < 0) diagonalSpeed *= -1;
+  if (diagonalY > 400 || diagonalY < 0) diagonalSpeed *= -1;
   fill(0);
-  textSize(32);
+  textSize(titleSize);
   textAlign(CENTER, TOP);
   text("SELF PORTRAIT", width / 2, height / 10);
+  if (frameCount % 60 == 0) { 
+    if (titleSize < 160) {
+      titleSize += 8;
+    } else {
+      titleSize -= 8;
+    }
+  }
 }
-
